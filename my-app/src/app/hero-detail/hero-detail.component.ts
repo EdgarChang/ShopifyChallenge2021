24f77fd1e12a165ee0a111af4e7eff3c943b3a46
Edgar Chang <tsung-hsun.chang@mail.mcgill.ca>
2021-01-17 01:06:28 +0800

    fix github page

--- my-app/src/app/hero-detail/hero-detail.component.ts
+++ my-app/src/app/hero-detail/hero-detail.component.ts
@@ -23,6 +23,14 @@ export class HeroDetailComponent implements OnInit {
   	this.movies.push(movie);
   }
 
+  remove(movie:Hero) {
+  	this.movies = this.movies.filter(entry => entry !== movie);
+  }
+
+  nominated(movie:Hero):boolean {
+  	//console.log(this.movies.indexOf({year:movie.year,title:movie.title}));
+  	return this.movies.find(e => e.title===movie.title && e.year===movie.year)!=null;
+  }
   
 
 }
